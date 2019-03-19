ef6b5c0321c9b43c8c34a333153ab823389ea915
jordanstanding <pensandneedlesco@gmail.com>
2019-03-18 22:38:00 -0400

    update

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-// $(document).ready(function() {
+$(document).ready(function() {
+   $(function() {
+        $('.lazy').Lazy();
+    });
 
-//     // $(function() {
-//     //     $('.lazy').lazy();
-//     // });
-
-
-//   $('a').on('click', function(ev) {
-//     ev.preventDefault();
-//   });
-
-// });
+   $('.colapse-nav img').on('click', function() {
+    $(this).next().fadeToggle('slow', function() {
+      if($('.int .flex-col').is(':visible')) {
+        if($(this).attr('data-nav') == 'starry') {
+          $('.colapse-nav .starry').attr('src', 'images/StarryEyedHeader_open.jpg');
+        } else if($(this).attr('data-nav') == 'hotel') {
+          $('.colapse-nav .hotel').attr('src', 'images/HotelBisousHeader_open.jpg');
+        }
+      } else if($('.int .flex-col').is(':hidden')){
+        if($(this).attr('data-nav') == 'hotel') {
+          $('.colapse-nav .hotel ').attr('src', 'images/HotelBisousHeader.jpg');
+        } else if($(this).attr('data-nav') == 'starry'){
+          $('.colapse-nav .starry ').attr('src', 'images/StarryEyedHeader.jpg');
+        }
+      }
+    });
+   });
+});
