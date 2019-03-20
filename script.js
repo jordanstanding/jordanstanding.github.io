d3d02fbdefa6c2ee70ca02a2698a5be2608ab480
jordanstanding <pensandneedlesco@gmail.com>
2019-03-19 20:42:17 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ $(document).ready(function() {
         $('.lazy').Lazy();
     });
 
-   // $('.colapse-nav img').on('click', function() {
-   //  $(this).next().fadeToggle('slow', function() {
-   //    if($('.int .flex-col').is(':visible')) {
-   //      if($(this).attr('data-nav') == 'starry') {
-   //        $('.colapse-nav .starry').attr('src', 'images/StarryEyedHeader_open.jpg');
-   //      } else if($(this).attr('data-nav') == 'hotel') {
-   //        $('.colapse-nav .hotel').attr('src', 'images/HotelBisousHeader_open.jpg');
-   //      }
-   //    } else if($('.int .flex-col').is(':hidden')){
-   //      if($(this).attr('data-nav') == 'hotel') {
-   //        $('.colapse-nav .hotel ').attr('src', 'images/HotelBisousHeader.jpg');
-   //      } else if($(this).attr('data-nav') == 'starry'){
-   //        $('.colapse-nav .starry ').attr('src', 'images/StarryEyedHeader.jpg');
-   //      }
-   //    }
-   //  });
-   // });
+   $('.colapse-nav img').on('click', function() {
+    $(this).next().fadeToggle('slow', function() {
+      if($('.int .flex-col').is(':visible')) {
+        if($(this).attr('data-nav') == 'starry') {
+          $('.colapse-nav .starry').attr('src', 'images/StarryEyedHeader_open.jpg');
+        } else if($(this).attr('data-nav') == 'hotel') {
+          $('.colapse-nav .hotel').attr('src', 'images/HotelBisousHeader_open.jpg');
+        }
+      } else if($('.int .flex-col').is(':hidden')){
+        if($(this).attr('data-nav') == 'hotel') {
+          $('.colapse-nav .hotel ').attr('src', 'images/HotelBisousHeader.jpg');
+        } else if($(this).attr('data-nav') == 'starry'){
+          $('.colapse-nav .starry ').attr('src', 'images/StarryEyedHeader.jpg');
+        }
+      }
+    });
+   });
 });
