cb91e58025bc30b2eb307d71f9014304e631411a
jordanstanding <pensandneedlesco@gmail.com>
2019-03-20 22:48:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ $(document).ready(function() {
         $('.lazy').Lazy();
     });
 
+  // nav stuff
+  /* Open when someone clicks on the span element */
+  $('.open').on('click', function() {
+    if($(window).width() > 768) {
+      $("#myNav").css('width', '60%');
+    } else {
+      $("#myNav").css('width', '100%');
+    }
+  })
+
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  $('.closebtn').on('click', function() {
+    $("#myNav").css('width', '0%');
+  });
+
    $('.colapse-nav img').on('click', function() {
     $(this).next().fadeToggle('slow', function() {
       if($('.int .flex-col').is(':visible')) {
@@ -10,12 +25,16 @@ $(document).ready(function() {
           $('.colapse-nav .starry').attr('src', 'images/StarryEyedHeader_open.jpg');
         } else if($(this).attr('data-nav') == 'hotel') {
           $('.colapse-nav .hotel').attr('src', 'images/HotelBisousHeader_open.jpg');
+        } else if($(this).attr('data-nav') == 'marigold') {
+          $('.colapse-nav .marigold').attr('src', 'images/Marigold_minus.jpg');
         }
       } else if($('.int .flex-col').is(':hidden')){
         if($(this).attr('data-nav') == 'hotel') {
           $('.colapse-nav .hotel ').attr('src', 'images/HotelBisousHeader.jpg');
         } else if($(this).attr('data-nav') == 'starry'){
           $('.colapse-nav .starry ').attr('src', 'images/StarryEyedHeader.jpg');
+        } else if($(this).attr('data-nav') == 'marigold'){
+          $('.colapse-nav .marigold ').attr('src', 'images/Marigold_plus.jpg');
         }
       }
     });
